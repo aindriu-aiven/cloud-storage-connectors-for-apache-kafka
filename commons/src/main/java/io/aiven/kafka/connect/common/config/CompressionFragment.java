a3ab8834aefb23439c4e2502d5d376586fff469f
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2024-11-12 17:04:35 +0000

    merge common config fragments
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- commons/src/main/java/io/aiven/kafka/connect/common/config/CompressionFragment.java
+++ commons/src/main/java/io/aiven/kafka/connect/common/config/CompressionFragment.java
@@ -1,23 +1,40 @@
+/*
+ * Copyright 2024 Aiven Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.aiven.kafka.connect.common.config;
 
-import io.aiven.kafka.connect.common.config.validators.FileCompressionTypeValidator;
+import java.util.Objects;
+
 import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 
-import java.util.Objects;
+import io.aiven.kafka.connect.common.config.validators.FileCompressionTypeValidator;
 
 public class CompressionFragment {
 
     private static final String GROUP_COMPRESSION = "File Compression";
     private static final String FILE_COMPRESSION_TYPE_CONFIG = "file.compression.type";
 
-    private AbstractConfig cfg;
+    private final AbstractConfig cfg;
 
-    public CompressionFragment(AbstractConfig cfg) {
+    public CompressionFragment(final AbstractConfig cfg) {
         this.cfg = cfg;
     }
 
-    public static ConfigDef update(final ConfigDef configDef,  final CompressionType defaultCompressionType) {
+    public static ConfigDef update(final ConfigDef configDef, final CompressionType defaultCompressionType) {
         configDef.define(FILE_COMPRESSION_TYPE_CONFIG, ConfigDef.Type.STRING,
                 Objects.isNull(defaultCompressionType) ? null : defaultCompressionType.name, // NOPMD NullAssignment
                 new FileCompressionTypeValidator(), ConfigDef.Importance.MEDIUM,
--- commons/src/test/java/io/aiven/kafka/connect/common/config/FileNameFragmentTest.java
+++ commons/src/test/java/io/aiven/kafka/connect/common/config/FileNameFragmentTest.java
@@ -1,22 +1,48 @@
+/*
+ * Copyright 2024 Aiven Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.aiven.kafka.connect.common.config;
 
-public class FileNameFragmentTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class FileNameFragmentTest {// NOPMD
 
     public enum FileNameArgs {
-    GROUP_FILE(FileNameFragment.GROUP_FILE), FILE_COMPRESSION_TYPE_CONFIG(FileNameFragment.FILE_COMPRESSION_TYPE_CONFIG),
-    FILE_MAX_RECORDS (FileNameFragment.FILE_MAX_RECORDS),
-    FILE_NAME_TIMESTAMP_TIMEZONE (FileNameFragment.FILE_NAME_TIMESTAMP_TIMEZONE),
-    FILE_NAME_TIMESTAMP_SOURCE (FileNameFragment.FILE_NAME_TIMESTAMP_SOURCE),
-    FILE_NAME_TEMPLATE_CONFIG (FileNameFragment.FILE_NAME_TEMPLATE_CONFIG),
-    DEFAULT_FILENAME_TEMPLATE (FileNameFragment.DEFAULT_FILENAME_TEMPLATE);
-    String key;
-    FileNameArgs(String key){
-        this.key=key;
+        GROUP_FILE(FileNameFragment.GROUP_FILE), FILE_COMPRESSION_TYPE_CONFIG(
+                FileNameFragment.FILE_COMPRESSION_TYPE_CONFIG), FILE_MAX_RECORDS(
+                        FileNameFragment.FILE_MAX_RECORDS), FILE_NAME_TIMESTAMP_TIMEZONE(
+                                FileNameFragment.FILE_NAME_TIMESTAMP_TIMEZONE), FILE_NAME_TIMESTAMP_SOURCE(
+                                        FileNameFragment.FILE_NAME_TIMESTAMP_SOURCE), FILE_NAME_TEMPLATE_CONFIG(
+                                                FileNameFragment.FILE_NAME_TEMPLATE_CONFIG), DEFAULT_FILENAME_TEMPLATE(
+                                                        FileNameFragment.DEFAULT_FILENAME_TEMPLATE);
+        final String key;// NOPMD
+        FileNameArgs(final String key) {
+            this.key = key;
+        }
+
+        public String key() {
+            return key;
+        }
     }
 
-    public String key() {
-        return key;
+    @Test
+    public void testEnum() {// NOPMD
+        assertThat(FileNameArgs.GROUP_FILE.key).isEqualTo(FileNameFragment.GROUP_FILE);
     }
-}
 
 }
